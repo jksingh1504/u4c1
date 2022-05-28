41fb0733f9b0d0b8e5b26d0df7d66c584c182c1c
jksingh1504 <boby150499@gmail.com>
2022-05-28 15:32:58 +0530

    hello

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import styles from "./tasks.module.css";
 import image from "../../assets/empty.svg";
-import Task from "../Task/Task"
+import Task from "../Task/Task";
 
-const Tasks = ({todo}) => {
-  // console.log(todo)
-  // NOTE: do not delete `data-cy` key value pair
-  return (
-    <>
-      <ul data-cy="tasks" className={styles.tasks}>
-        {/* Task List */}
-        {
-          todo.map(ele=>
-            <Task task={ele} key={ele.id}/>
-          )
-        }
-      </ul>
-      
-      <div data-cy="tasks-empty" className={styles.empty}>
-        {/* Show when No Tasks are present */}
-        <img src={image} alt="" />
-        <h3>Empty list</h3>
-        <p>Add a new task above</p>
-      </div>
-    </>
-  );
+const Tasks = ({ todo }) => {
+	// console.log(todo)
+	// NOTE: do not delete `data-cy` key value pair
+	if (todo.length > 0) {
+		return (
+			<ul data-cy="tasks" className={styles.tasks}>
+				{/* Task List */}
+				{todo.map((ele) => (
+					<Task task={ele} key={ele.id} />
+				))}
+			</ul>
+		);
+	}
+
+	return (
+		<div data-cy="tasks-empty" className={styles.empty}>
+			{/* Show when No Tasks are present */}
+			<img src={image} alt="" />
+			<h3>Empty list</h3>
+			<p>Add a new task above</p>
+		</div>
+	);
 };
 
 export default Tasks;
